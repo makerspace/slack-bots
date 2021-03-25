c731b17fd56f69e5b2062593a80524fe64dd8f9a
Daniel Berglund <daniel.k.berglund@gmail.com>
2021-03-25 21:13:05 +0100

    Fixed so that the bot doesn't react incorrectly

--- plugins/faqbot.py
+++ plugins/faqbot.py
@@ -5,7 +5,23 @@ import re
 class AnswerFAQPlugin(MachineBasePlugin):
     commandToken = '!'
 
-    @listen_to(regex=r'^'+re.escape(commandToken)+'\s*faq')
+    @listen_to(regex=r'^'+re.escape(commandToken)+'\s*about\s*$')
+    def aboutQuestion(self, msg):
+        msgToSend="Vi har lite olika slack bottar.\nFAQ bot: "+self.commandToken+"aboutfaq"
+        if msg.in_thread or not msg.text[0] == self.commandToken:
+            msg.reply(msgToSend, in_thread=True)
+        else:
+            msg.say(msgToSend)
+
+    @listen_to(regex=r'^'+re.escape(commandToken)+'\s*aboutfaq\s*$')
+    def aboutFAQQuestion(self, msg):
+        msgToSend="Faq botten svarar på diverse frågor.\n !nyckel !box !faq"
+        if msg.in_thread or not msg.text[0] == self.commandToken:
+            msg.reply(msgToSend, in_thread=True)
+        else:
+            msg.say(msgToSend)
+
+    @listen_to(regex=r'^'+re.escape(commandToken)+'\s*faq\s*$')
     def faqQuestion(self, msg):
         msgToSend="Makerspace FAQ: https://wiki.makerspace.se/Makerspace_FAQ"
         if msg.in_thread or not msg.text[0] == self.commandToken:
@@ -14,7 +30,7 @@ class AnswerFAQPlugin(MachineBasePlugin):
             msg.say(msgToSend)
 
     @listen_to(regex=r'nyckelutlämning.*\?')
-    @listen_to(regex=r'^'+re.escape(commandToken)+'\s*nyckel')
+    @listen_to(regex=r'^'+re.escape(commandToken)+'\s*nyckel$')
     def keyQuestion(self, msg):
         msgToSend=":key: Du vill nog ha info om nyckelutlämningar. TBC :)"
         if msg.in_thread or not msg.text[0] == self.commandToken:
@@ -22,8 +38,8 @@ class AnswerFAQPlugin(MachineBasePlugin):
         else:
             msg.say(msgToSend)
 
-    @listen_to(regex=r'^'+re.escape(commandToken)+'\s*box')
-    @listen_to(regex=r'^'+re.escape(commandToken)+'\s*låda')
+    @listen_to(regex=r'^'+re.escape(commandToken)+'\s*box\s*$')
+    @listen_to(regex=r'^'+re.escape(commandToken)+'\s*låda\s*$')
     def boxQuestion(self, msg):
         msgToSend="Maximala måtten för labblåda är ca 50 x 39 x 26 cm. Mer info om förvaring på spacet och exempel på lådor finns på: https://wiki.makerspace.se/Makerspace_FAQ#F%C3%B6rvaring"
         if msg.in_thread or not msg.text[0] == self.commandToken:
