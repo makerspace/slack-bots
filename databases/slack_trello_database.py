3d05b02511f77f88e26dcfc51fb22aca45e94ccf
Daniel Berglund <daniel.k.berglund@gmail.com>
2021-04-20 21:03:52 +0200

    Added test for slack_trello_db. Fixed some bugs in slack_trello_db

--- databases/slack_trello_database.py
+++ databases/slack_trello_database.py
@@ -5,63 +5,97 @@ from machine.models.user import User as SlackUser
 from base.slack import Slack
 from base.trello import Trello
 
-#TODO move to settings file
-database_name = 'slack_trello'
-table_name = 'users'
-
 class SlackTrelloDB():
 
-    def __init__(self, slack:Slack, trello:Trello):
+    def __init__(self, slack:Slack, trello:Trello, database_name, table_name):
         self._slack = slack
         self._trello= trello
-        
+        self._database_name = database_name
+        self._table_name = table_name
+
         dbUser = os.getenv('MYSQL_BOT_USER')
         dbPassword = os.getenv('MYSQL_BOT_PASSWORD')
         if dbUser == None:
             raise RuntimeError('MYSQL_BOT_USER not set')
         if dbPassword == None:
-            raise RuntimeError('MYSQL_BOT_PASSWORD not set') 
-        
-        self._db = mysql.connector.connect(
-          host="localhost",
-          user=dbUser,
-          password=dbPassword,
-          database=database_name
-        )
+            raise RuntimeError('MYSQL_BOT_PASSWORD not set')
+
+        self._db_config = {
+            'host': "localhost",
+            'user': dbUser,
+            'password': dbPassword,
+            'database': self._database_name
+        }
+
+    def _openDB(self):
+        self._db = mysql.connector.connect(**self._db_config)
         self._dbCursor = self._db.cursor()
-        print('Slack Trello database connection started')
+
+    def _closeDB(self):
+        self._dbCursor.close()
+        self._db.close()
 
     def addUser(self, slackUser : SlackUser, trelloUser: TrelloUser ):
         #TODO raise exception based on how it goes
-        
-        sql = "INSERT INTO "+table_name+" (slackUserName, slackID, trelloUserName, trelloID) VALUES (%s, %s, %s, %s)"
+        self._openDB()
+
+        sql = "INSERT INTO "+self._table_name+" (slackUserName, slackID, trelloUserName, trelloID) VALUES (%s, %s, %s, %s)"
         data = (slackUser.name, slackUser.id, trelloUser.username, trelloUser.id)
         self._dbCursor.execute(sql, data)
         self._db.commit()
+        self._closeDB()
 
-    def removeUser(self, slackUser : SlackUser):
-        print('remove user')
-        #TODO remove a user
-        #TODO  raise exception based on how it goes
+    def removeUserBySlack(self, slackUser : SlackUser):
+        #TODO raise exception based on how it goes
+        self._openDB()
+        print(slackUser.id)
+        print(slackUser.name)
+
+        sql = "DELETE FROM " + self._table_name + " WHERE slackID = %s"
+        data = (slackUser.id,)
+        self._dbCursor.execute(sql, data)
+        self._db.commit()
+
+        #sql = "SELECT * FROM " + self._table_name
+        #self._dbCursor.execute(sql)
+        #result = self._dbCursor.fetchall()
+        #result.close()
+        #print("A "+result)
+        self._closeDB()
+
+    def removeUserByTrello(self, trelloUser : TrelloUser):
+        #TODO raise exception based on how it goes
+        self._openDB()
+        sql = "DELETE FROM " + self._table_name + " WHERE trelloID = %s"
+        data = (trelloUser.id,)
+        self._dbCursor.execute(sql, data)
+        self._db.commit()
+        self._closeDB()
 
     def getSlackUser(self, trelloUser: TrelloUser):
         #TODO raise exception based on how it goes
-        
-        sql = "SELECT * FROM "+table_name+" WHERE trelloID = %s"
+        self._openDB()
+        sql = "SELECT * FROM "+self._table_name+" WHERE trelloID = %s"
         data = (trelloUser.id,)
         self._dbCursor.execute(sql, data)
         result = self._dbCursor.fetchall()
-        
-        #TODO deal with result
-        return self._slack.getSlackUserByID()
+        self._closeDB()
+
+        if len(result) != 0:
+            raise RunetimeError("Database error, multiple users with same ID.")
+        slack_id = result[0][4]
+        return self._slack.getSlackUserByID(slack_id)
 
     def getTrelloUser(self, slackUser : SlackUser):
         #TODO raise exception based on how it goes
-        
-        sql = "SELECT * FROM "+table_name+" WHERE slackID = %s"
-        data = (slackUser.id, )
+        self._openDB()
+        sql = "SELECT * FROM "+self._table_name+" WHERE slackID = %s"
+        data = (slackUser.id,)
         self._dbCursor.execute(sql, data)
         result = self._dbCursor.fetchall()
-        
-        #TODO deal with result
-        return self._trello.getUserByID()
+        self._closeDB()
+
+        if len(result) != 0:
+            raise RunetimeError("Database error, multiple users with same ID.")
+        trello_id = result[0][2]
+        return self._trello.getUserByID(trello_id)
---
+++ tests/test_slack_trello_db.py
@@ -0,0 +1,134 @@
+import re, os, pytest, mock, mysql.connector
+from pytest_mock import mocker
+
+from databases.slack_trello_database import SlackTrelloDB
+from tests.mocks.slack_user import SlackUserMock
+from tests.mocks.trello_user import TrelloUserMock
+
+class TestSlackTrelloDB:
+
+    #Assumes that the datbase db_name exists or the bot user has permission to create it
+    db_name = "slack_trello_testdb"
+    table_name = "test_table"
+
+    def _openDB(self):
+        self._db = mysql.connector.connect(**self._db_config)
+        self._dbCursor = self._db.cursor()
+
+    def _closeDB(self):
+        self._dbCursor.close()
+        self._db.close()
+
+    def check_users(self, slack_users, trello_users):
+        self._openDB()
+        sql = "SELECT * FROM " + self.table_name
+        self._dbCursor.execute(sql)
+        result = self._dbCursor.fetchall()
+        self._closeDB()
+
+        if len(slack_users) != len(trello_users):
+            assert False, "number of users missmatch "+str(len(slack_users)) + ","+str(len(trello_users))
+
+        if len(result) != len(slack_users):
+            assert False, " numbers of users("+str(len(slack_users))+") does not match with number of users in database("+str(len(result))+")"
+
+        for i in range(len(result)):
+            assert result[i][1] == str(slack_users[i].name)
+            assert result[i][2] == str(slack_users[i].id)
+            assert result[i][3] == str(trello_users[i].username)
+            assert result[i][4] == str(trello_users[i].id)
+
+    @pytest.fixture()
+    def setup_teardown(self):
+        #Get the SQL user information and set the config
+        dbUser = os.getenv('MYSQL_BOT_USER')
+        dbPassword = os.getenv('MYSQL_BOT_PASSWORD')
+        if dbUser == None:
+            raise RuntimeError('MYSQL_BOT_USER not set')
+        if dbPassword == None:
+            raise RuntimeError('MYSQL_BOT_PASSWORD not set') 
+
+        self._db_config = {
+            "host": "localhost",
+            "user": dbUser,
+            "password": dbPassword,
+            "database": self.db_name
+        }
+    
+        #Setup
+        self._openDB()
+        self._dbCursor.execute("DROP TABLE IF EXISTS "+self.table_name)
+        self._db.commit()
+        self._dbCursor.execute("CREATE DATABASE IF NOT EXISTS "+self.db_name)
+        self._dbCursor.execute("CREATE TABLE "+self.table_name+" (id INT AUTO_INCREMENT PRIMARY KEY, slackUserName VARCHAR(30), slackID VARCHAR(255), trelloUserName VARCHAR(30), trelloID VARCHAR(255))")
+        self._db.commit()
+        self._closeDB()
+
+        yield self._db
+
+        #Teardown
+        self._openDB()
+        self._dbCursor.execute('DROP TABLE '+self.table_name)
+        self._db.commit()
+        self._closeDB()
+
+    @pytest.fixture
+    def init(self, request):
+        slack_trello = SlackTrelloDB("a", "a", self.db_name, self.table_name)
+        slack_users = []
+        trello_users = []
+        for i in range(request.param):
+            slack_user = SlackUserMock(str(i)+" Slack", 100+i)
+            trello_user = TrelloUserMock(str(i)+" Trello", 200+i)
+            slack_users.append(slack_user)
+            trello_users.append(trello_user)
+            slack_trello.addUser(slack_user, trello_user)
+        return slack_trello, slack_users, trello_users
+
+    @pytest.mark.parametrize("init", [1], indirect=["init"]) #TODO 5
+    def test_add_user(self, setup_teardown, init):
+        slack_trello_DB, slack_users, trello_users = init
+        self.check_users(slack_users, trello_users)
+        
+        slack_user = SlackUserMock("A Slack", 1)
+        trello_user = TrelloUserMock("B Trello", 2)
+        slack_users.append(slack_user)
+        trello_users.append(trello_user)
+        slack_trello_DB.addUser(slack_user,trello_user)
+        self.check_users(slack_users, trello_users)
+
+    @pytest.mark.parametrize("init", [5], indirect=["init"])
+    def test_remove_user_slack(self, setup_teardown, init):
+        slack_trello_DB, slack_users, trello_users = init
+        self.check_users(slack_users, trello_users)
+
+        index = 1
+        slack_trello_DB.removeUserBySlack(slack_users[index])
+        slack_users.remove(slack_users[index])
+        trello_users.remove(trello_users[index])
+        self.check_users(slack_users, trello_users)
+
+        for i in range(len(slack_users)):
+            slack_trello_DB.removeUserBySlack(slack_users[i])
+        self.check_users([], [])
+
+    @pytest.mark.parametrize("init", [5], indirect=["init"])
+    def test_remove_user_trello(self, setup_teardown, init):
+        slack_trello_DB, slack_users, trello_users = init
+        self.check_users(slack_users, trello_users)
+
+        #slack_trello_DB.removeUserByTrello()
+
+    @pytest.mark.parametrize("init", [5], indirect=["init"])
+    def test_get_slack_user(self, setup_teardown, init): #TODO mock getSlackUserByID
+        slack_trello_DB, slack_users, trello_users = init
+        self.check_users(slack_users, trello_users)
+
+        #TODO get stuff
+
+    @pytest.mark.parametrize("init", [5], indirect=["init"])
+    def test_get_trello_user(self, setup_teardown, init): #TODO mock getTrelloUserByID
+        slack_trello_DB, slack_users, trello_users = init
+        self.check_users(slack_users, trello_users)
+
+        #TODO get stuff
